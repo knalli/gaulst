7bb2bc5c5dc473a88e3c153228bc5a261b433547
Jan Philipp <knallisworld@googlemail.com>
2007-04-18 15:13:17 +0000

    Javadoc hinzugefügt.

--- schach/src/schach/brett/Linie.java
+++ schach/src/schach/brett/Linie.java
@@ -1,5 +1,10 @@
 package schach.brett;
 
+/**
+ * Eine Linie auf einem Schachbrett (A bis H).
+ * @author Jan Philipp
+ *
+ */
 public enum Linie {
 	A,B,C,D,E,F,G,H
 }
--- schach/src/schach/brett/Reihe.java
+++ schach/src/schach/brett/Reihe.java
@@ -1,5 +1,10 @@
 package schach.brett;
 
+/**
+ * Eine Reihe auf einem Schachbrett (1-8).
+ * @author Jan Philipp
+ *
+ */
 public enum Reihe {
 	R1,R2,R3,R4,R5,R6,R7,R8
 }
