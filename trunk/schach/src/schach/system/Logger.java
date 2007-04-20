f873d9ba5f2d1934e055e7f175554ee226bcf51d
knallisworld <knallisworld@7f2e3a59-512e-0410-9e9f-1bacee8680c6>
2007-04-20 17:33:25 +0000

    * Auto-Generierung der IFigur-Schnittstellen (aus OCL) * Auto-Generierung der Figurklassen (nur Pflichtanteile eingebunden, keine Implementationen) * Testfall eingebaut (VIewtest)

---
+++ trunk/schach/src/schach/system/Logger.java
@@ -0,0 +1,39 @@
+package schach.system;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import schach.system.internal.ConsoleLogger;
+
+public class Logger {
+	private static List<ILogger> loggers = new ArrayList<ILogger>();
+	
+	public static void appendLogger(String name){
+		if(name.equals("console"))
+			loggers.add(new ConsoleLogger());
+	}
+	
+	public static void info(String string){
+		for(ILogger logger : loggers){
+			logger.info(string);
+		}
+	}
+	
+	public static void error(String string){
+		for(ILogger logger : loggers){
+			logger.error(string);
+		}
+	}
+	
+	public static void warning(String string){
+		for(ILogger logger : loggers){
+			logger.warning(string);
+		}
+	}
+	
+	public static void debug(String string){
+		for(ILogger logger : loggers){
+			logger.debug(string);
+		}
+	}
+}
