71279a0bac43060159d6d2095c8414ee25b91568
Jan Philipp <knallisworld@googlemail.com>
2007-04-25 10:53:33 +0000

    Multifunktionsview (Text & Gui gleichzeitig)

--- schach/src/schach/system/internal/GuiView.java
+++ schach/src/schach/system/internal/GuiView.java
@@ -345,7 +345,7 @@ public class GuiView implements IView {
 		besetzteFelder.clear();
 		for(IFigur figur : allefiguren.gebeFiguren(listeFigurarten,listeFarben)){
 			besetzteFelder.put(figur.gebePosition(),figur);
-			Logger.debug("View: "+figur+" steht auf "+figur.gebePosition());
+//			Logger.debug("View: "+figur+" steht auf "+figur.gebePosition());
 		}
 		
 		// zeichne Brett
