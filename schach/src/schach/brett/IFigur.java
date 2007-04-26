006643edcde1ad82360f8a74e70a8717fb216396
Jan Philipp <knallisworld@googlemail.com>
2007-04-26 13:29:05 +0000

    Füge zieht und schlägt ein.. das kann ja jede Figur!

--- schach/src/schach/brett/IFigur.java
+++ schach/src/schach/brett/IFigur.java
@@ -66,4 +66,18 @@ public interface IFigur {
 	 */
 	public Figurart gebeArt();
 	
+	/**
+	 * Die Figur zieht auf das <code>ziel</code>-Feld.
+	 * @param ziel
+	 * @throws NegativeConditionException
+	 */
+	public void zieht(IFeld ziel) throws NegativeConditionException;
+	
+	/**
+	 * Die Figur zieht auf das <code>ziel</code>-Feld und schlägt dabei die gegnerische <code>gegner</code>-Figur.
+	 * @param ziel Zielfeld
+	 * @param gegner gegnerische Figur
+	 * @throws NegativeConditionException
+	 */
+	public void schlaegt(IFeld ziel, ISchlagbareFigur gegner) throws NegativeConditionException;
 }
