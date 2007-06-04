55f7e8965612cfe5065c4b801dea0ce34620dd78
knallisworld <knallisworld@7f2e3a59-512e-0410-9e9f-1bacee8680c6>
2007-06-04 20:35:08 +0000

    Logger/test

--- trunk/schach/src/schach/system/internal/ConsoleLogger.java
+++ trunk/schach/src/schach/system/internal/ConsoleLogger.java
@@ -5,28 +5,38 @@ import java.io.PrintStream;
 import schach.system.ILogger;
 
 public class ConsoleLogger implements ILogger {
+	
+	private boolean onlytest = false;
 
 	private void print(PrintStream out, String string){
 		out.println(string);
 	}
 	
 	public void debug(String string) {
-		print(System.out, "DEBUG: "+string);
+		if(!onlytest)
+			print(System.out, "DEBUG: "+string);
 	}
 
 	public void error(String string) {
-		print(System.err, "ERROR: "+string);
+		if(!onlytest)
+			print(System.err, "ERROR: "+string);
 	}
 
 	public void info(String string) {
-		print(System.out, "INFO: "+string);
+		if(!onlytest)
+			print(System.out, "INFO: "+string);
 	}
 
 	public void warning(String string) {
-		print(System.err, "WARNING: "+string);
+		if(!onlytest)
+			print(System.err, "WARNING: "+string);
 	}
 
 	public void test(String string) {
 		print(System.out, "TEST: "+string);
+	}
+
+	public void disableAllExceptTest() {
+		onlytest = true;
 	}
 }
